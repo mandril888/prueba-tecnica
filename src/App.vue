a04a0dd19a94bafd6c140213525693d18a009f04
Arturo Rubio <arubio@seocom.agency>
2023-08-28 17:30:08 +0200

    fetch 100 users and add users to a table

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
+import getUsers from "./services/UsersRepository";
+import UsersList from "./components/UsersList.vue";
+import { ref } from "vue";
 
+const users = ref([]);
+getUsers().then((response) => {
+  users.value = response.results;
+})
 </script>
 
 <template>
@@ -8,35 +15,6 @@
   </header> -->
 
   <main>
-    <p>Add table</p>
+    <UsersList :users="users" />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,34 @@
+<template>
+  <table width="100%">
+    <thead>
+      <tr>
+        <th>Foto</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>País</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td><img :src="user.picture.thumbnail" :alt="user.name.first" /></td>
+        <td>{{ user.name.first }}</td>
+        <td>{{ user.name.last }}</td>
+        <td>{{ user.location.country }}</td>
+        <td><button>Borrar</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  users: Object
+})
+</script>
+
+<style scoped>
+th {
+  font-weight: 800;
+}
+</style>
